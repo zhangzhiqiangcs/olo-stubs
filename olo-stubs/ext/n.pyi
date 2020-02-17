d186a0b0de51df2c2fc9abd27a8ccaf763f91b92
yetone <yetoneful@gmail.com>
2020-02-17 15:18:06 +0800

    fix: overload ext.n

--- olo-stubs/ext/n.pyi
+++ olo-stubs/ext/n.pyi
@@ -1,5 +1,9 @@
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, overload
 
 T = TypeVar('T')
 
+@overload
 def n(default: Callable[..., T]) -> Callable[..., T]: ...
+
+@overload
+def n(default: T) -> Callable[..., T]: ...
