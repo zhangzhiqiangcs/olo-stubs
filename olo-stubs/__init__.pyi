040ca27497d95b2f5521420d6acdd7c2400b43c7
yetone <yetoneful@gmail.com>
2020-10-15 12:50:44 +0800

    feat: support batch field

--- olo-stubs/__init__.pyi
+++ olo-stubs/__init__.pyi
@@ -1,4 +1,4 @@
-from .field import Field as Field, DbField as DbField, UnionField as UnionField, ConstField as ConstField, JSONField as JSONField
+from .field import Field as Field, DbField as DbField, BatchField as BatchField, UnionField as UnionField, ConstField as ConstField, JSONField as JSONField
 from .model import Model as Model
 from .ast_api import select as select
 from .database import DataBase as DataBase
